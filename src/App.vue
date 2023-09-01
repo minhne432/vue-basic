64f78f4415f8707e1095d448bd9d81f9de3cfe1b
minhne432 <nh.minh0403@gmail.com>
2023-09-01 16:53:32 +0700

    hello

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,7 @@
 <template>
-  <h1>{{ title }} {{ Name }} {{ age }} years old!</h1>
-  <button v-on:click="changeTiltle('GoodBye')">click here</button>
-  <br />
-  <input type="text" @input="changeName($event)" />
-  <input type="text" @input="changeAge($event)" />
-  <br />
-  <input type="text" @keyup.enter="changeName($event)" />
-  <input type="text" @keyup.enter="changeAge($event)" />
-  <br />
-  <form action="" @submit.prevent="formSubmit">
-    <input type="text" v-model="Name" />
-    <input type="text" v-model="age" />
-
-    <button>Submit</button>
-  </form>
+  <h1>Hello</h1>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      title: "Hello",
-      Name: "Minh",
-      age: 20,
-    };
-  },
-  methods: {
-    changeTiltle(my_title) {
-      this.title = my_title;
-    },
-    changeName(event) {
-      this.Name = event.target.value;
-    },
-    changeAge(event) {
-      this.age = event.target.value;
-    },
-    update_inf(my_name, my_Age) {
-      this.changeName(my_name);
-      this.changeAge(my_Age);
-    },
-    formSubmit() {
-      alert(this.Name);
-      alert(this.age);
-    },
-  },
-};
+export default {};
 </script>
